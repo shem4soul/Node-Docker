ed598e8de9667c5df89f01bb4005a1e343360872
Shittu Emmanuel <shem4soul@gmail.com>
2026-08-01 20:17:29 +0100

    add Dockerfile and index.js for initial Node.js application setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${port}`);
+});
